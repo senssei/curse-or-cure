c5202a56c6900ec2a60d68d00dc475d4e88e19ef
Rafał Warzycha <rafal.warzycha@ts.fujitsu.com>
2016-11-22 12:23:27 +0100

    Interface avaiable only on compilation

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,6 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
 declare function greeter(person: string): string;
 declare var user: string;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+
 function greeter(person: string) {
     return "Hello, " + person;
 }
