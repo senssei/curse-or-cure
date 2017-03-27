7fe7205482ab865ac4b7c94264731a9f961e08e9
Rafał Warzycha <rafal.warzycha@gmail.com>
2017-03-27 11:37:09 +0200

    Lint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import * as _ from 'lodash';
+interface Sample{
 
-var characters = [
-  { 'name': 'barney', 'age': 36, 'blocked': false },
-  { 'name': 'fred',   'age': 40, 'blocked': true }
-];
+}
 
-// using "_.filter" callback shorthand
-let filtered = _.filter(characters, { 'age': 36 });
-
-console.log(filtered);
