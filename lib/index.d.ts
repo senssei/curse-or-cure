4cafa58d65c3077666d2e9c24d325128cf65bd20
Rafał Warzycha <rafal.warzycha@gmail.com>
2017-03-27 10:59:38 +0200

    Sample ts class

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,3 +2,13 @@ interface Person {
     firstName: string;
     lastName: string;
 }
+declare class Student implements Person {
+    firstName: any;
+    middleInitial: any;
+    lastName: any;
+    private _fullName;
+    readonly fullName: string;
+    constructor(firstName: any, middleInitial: any, lastName: any);
+}
+declare function greeter(person: Person): string;
+declare var user: Person;
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,25 @@ interface Person {
     firstName: string;
     lastName: string;
 }
+
+class Student implements Person {
+
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    constructor(public firstName, public middleInitial, public lastName) {
+        this._fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + ' ' + person.lastName;
+}
+
+var user: Person = new Student("Jane", "M.", "User");
+
+console.log(greeter(user));   
